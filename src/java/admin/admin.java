d2088dc6cff85d47942dc1585c542e41dc364fb7
riham <mohomedriham.7000@gmail.com>
2023-03-31 12:33:07 +0530

    this is the version 1.3 of the api application and this version includes new features. 1, admin can add product to the system 2, admin can change the details of all of the products to new details

--- src/java/admin/admin.java
+++ src/java/admin/admin.java
@@ -1,4 +1,4 @@
-/*
+    /*
  * To change this license header, choose License Headers in Project Properties.
  * To change this template file, choose Tools | Templates
  * and open the template in the editor.
@@ -87,5 +87,123 @@ public String getCustomers() {
     return result.toString();
 }
 
+    /**
+     * Web service operation
+     */
+    @WebMethod(operationName = "AddProduct")
+    public String AddProduct(@WebParam(name = "ProductName") String ProductName, @WebParam(name = "ProductPrice") int ProductPrice, @WebParam(name = "Quantity") int Quantity) {
+String result = "";
+    try {
+        Class.forName("com.microsoft.sqlserver.jdbc.SQLServerDriver");
+        String url = "jdbc:sqlserver://DESKTOP-37MVMA7\\SQLEXPRESS:1433; databaseName=details;  encrypt=true; trustServerCertificate=true; integratedSecurity=true";
+        Connection conn = DriverManager.getConnection(url);
+                 PreparedStatement insertStmt = conn.prepareStatement("INSERT INTO product (productname, productprice, quantity) VALUES (?, ?, ?)");
+                 insertStmt.setString(1, ProductName);
+                 insertStmt.setInt(2, ProductPrice);
+                 insertStmt.setInt(3, Quantity);
+                 int rows = insertStmt.executeUpdate();
+                 if (rows > 0) {
+                     result = "inserting product successful!";
+                 } else {
+                     result = "inserting product  failed!";
+                 }
+                 insertStmt.close();
+        } catch (SQLException e) {
+            System.err.println("D'oh! Got an exception!");
+            System.err.println(e.getMessage());
+            result = "Something went wrong";
+        
+    } catch (ClassNotFoundException e) {
+        System.err.println("D'oh! Got an exception!");
+        System.err.println(e.getMessage());
+        result = "Something went wrong";
+    }
+
+return result;
+    }
+
+@WebMethod(operationName = "getProducts")
+public String getProducts() {
+    String url = "jdbc:sqlserver://DESKTOP-37MVMA7\\SQLEXPRESS:1433; databaseName=details;  encrypt=true; trustServerCertificate=true; integratedSecurity=true";
+    Connection con = null;
+    Statement stmt = null;
+    ResultSet rs = null;
+    StringBuilder result = new StringBuilder();
+
+    try {
+        Class.forName("com.microsoft.sqlserver.jdbc.SQLServerDriver");
+        con = DriverManager.getConnection(url);
+        String query = "SELECT * FROM product ";
+        stmt = con.createStatement();
+        rs = stmt.executeQuery(query);
+        while (rs.next()) {
+            // Append each row of data to the result string
+            result.append("productname: ").append(rs.getString("productname")).append(", ")
+                  .append("productprice: ").append(rs.getString("productprice")).append(", ")
+
+                  .append("quantity: ").append(rs.getString("quantity")).append("\n");
+        }
+    } catch (Exception e) {
+        e.printStackTrace();
+    } finally {
+        try { if (rs != null) rs.close(); } catch (Exception e) { e.printStackTrace(); }
+        try { if (stmt != null) stmt.close(); } catch (Exception e) { e.printStackTrace(); }
+        try { if (con != null) con.close(); } catch (Exception e) { e.printStackTrace(); }
+    }
+    
+    return result.toString();
+}
+
+    /**
+     * Web service operation
+     */
+    @WebMethod(operationName = "EditProduct")
+    public String EditProduct(@WebParam(name = "pname") String pname, @WebParam(name = "pprice") int pprice, @WebParam(name = "quantity") int quantity) {
+        String url = "jdbc:sqlserver://DESKTOP-37MVMA7\\SQLEXPRESS:1433; databaseName=details;  encrypt=true; trustServerCertificate=true; integratedSecurity=true";
+    Connection con = null;
+    PreparedStatement pst = null;
+    ResultSet rs = null;
+    String result = ""; 
+    try {
+        Class.forName("com.microsoft.sqlserver.jdbc.SQLServerDriver");
+      con = DriverManager.getConnection(url);
+        String query = "SELECT * FROM product WHERE productname = ?";
+      pst = con.prepareStatement(query);
+        pst.setString(1, pname); // Set the username parameter
+        rs = pst.executeQuery();
+if (rs.next()) {
+    String productname = rs.getString("productname");
+
+    // Update the product's price and quantity using an SQL UPDATE statement
+    String updateQuery = "UPDATE product SET productprice = ?, quantity = ? WHERE productname = ?";
+    pst = con.prepareStatement(updateQuery);
+    pst.setInt(1, pprice); // Set the new price parameter
+    pst.setInt(2, quantity); // Set the new quantity parameter
+    pst.setString(3, productname); // Set the product ID parameter
+    int rowsUpdated = pst.executeUpdate();
+
+    // Check if any rows were updated successfully
+    if (rowsUpdated > 0) {
+        result = "Product updated successfully";
+    } else {
+        result = "Failed to update product";
+    }
+}else {
+result = "Invalid product name";
+       }
+         } catch (SQLException e) {
+            System.err.println("D'oh! Got an exception!");
+            System.err.println(e.getMessage());
+            result = "Something went wrong";
+        
+    } catch (ClassNotFoundException e) {
+        System.err.println("D'oh! Got an exception!");
+        System.err.println(e.getMessage());
+        result = "Something went wrong";
+    }
+    return result;
+}
+
+    
 
 }//end class
